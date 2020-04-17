dba3293e964b6fc559cd36ad3b1a96d795586308
BinhHust <ekkocat55@gmail.com>
2020-04-17 16:30:15 +0700

    Building the hero section

--- js/main.js
+++ js/main.js
@@ -1,38 +1,37 @@
 window.addEventListener("load", function () {
   new Glider(document.querySelector(".glider"), {
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 4,
+    slidesToScroll: 4,
     draggable: true,
     arrows: {
       prev: ".glider-prev",
       next: ".glider-next",
     },
-    responsive: [
-      {
-        breakpoint: 420,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          duration: 0.25,
-        },
-      },
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          duration: 0.25,
-          // itemWidth: 150,
-        },
-      },
-      {
-        breakpoint: 1000,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-          duration: 0.25,
-        },
-      },
-    ],
+    // responsive: [
+    //   {
+    //     breakpoint: 420,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 2,
+    //       duration: 0.25,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 800,
+    //     settings: {
+    //       slidesToShow: 3,
+    //       slidesToScroll: 3,
+    //       duration: 0.25,
+    //     },
+    //   },
+    //   {
+    //     breakpoint: 1000,
+    //     settings: {
+    //       slidesToShow: 4,
+    //       slidesToScroll: 4,
+    //       duration: 0.25,
+    //     },
+    //   },
+    // ],
   });
 });
